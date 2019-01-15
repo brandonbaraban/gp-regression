1a4ab0cc044012b194f4ce9ddbb1616b71799f15
Brandon Baraban <bbaraban@mit.edu>
2019-01-15 15:58:46 -0500

    reimplement gpr to be faster and more concise

--- gp_regression.py
+++ gp_regression.py
@@ -5,34 +5,21 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    #plot_sample_from_gp()
-    plot_MAP_from_gp_given_func(f=lambda x: np.sin(x), num_train=25)
+    plot_gpr_given_func(num_train=20)
 
 
-def plot_sample_from_gp(num_samples=1):
-    X = np.linspace(-5, 5, num=250)
-    gp = GPR()
+SQUARED_EXP = lambda l_scale, sigma: \
+              lambda x, y: \
+              sigma * np.exp(-(1 / (2 * np.power(l_scale, 2))) * np.power(np.linalg.norm(x - y), 2))
+def plot_gpr_given_func(f=lambda x: np.sin(x), num_train=5):
+    test_X = np.linspace(-5, 5, num=250) # 250 is arbitrary
+    X = np.random.choice(test_X, num_train, replace=False)
+    y = f(X)
     fig, ax = plt.subplots(1)
-    mean, covariance = gp.predict(X)
-    for i in range(num_samples):
-        Y = gp.sample(X)
-        ax.plot(X, Y, 'red')
-    std_dev = np.diagonal(covariance)
-    plot_confidence(X, mean, std_dev, ax)
-    plt.show()
-
-
-def plot_MAP_from_gp_given_func(f=lambda x: x, num_train=5):
-    X = np.linspace(-5, 5, num=250)
-    train_X = np.random.choice(X, num_train, replace=False)
-    train_Y = [f(x) for x in train_X]
-    gp = GPR(initial_dataset=(train_X, train_Y))
-    fig, ax = plt.subplots(1)
-    mean, covariance = gp.predict(X)
-    ax.plot(train_X, train_Y, 'b+')
-    ax.plot(X, mean, 'green')
-    std_devs = np.diagonal(covariance)
-    plot_confidence(X, mean, std_devs, ax)
+    ax.plot(X, y, 'b+')
+    mean, variance, log_ml = gpr(X, y, SQUARED_EXP(1, 1), 1e-2, test_X)
+    ax.plot(test_X, mean, 'red')
+    plot_confidence(test_X, mean, np.sqrt(variance), ax)
     plt.show()
 
 
@@ -40,104 +27,41 @@ def plot_confidence(X, mean, std_dev, ax):
     ax.fill_between(X, mean - 2 * std_dev, mean + 2 * std_dev, facecolor='pink', alpha=0.4)
 
 
-ZERO_MEAN = lambda x: 0
-ZERO_COVARIANCE = lambda x, y: int(x == y)
-SQUARED_EXP = lambda l_scale, sig_var: lambda x, y: sig_var * np.exp(-(1 / (2 * pow(l_scale, 2))) * pow(np.linalg.norm(x - y), 2))
-class GPR(object):
-    """ A representation of a Gaussian process for regression purposes.  """
+def gpr(inputs, targets, covariance_function, noise_level, test_inputs):
+    """
+    Implementation of Gaussian Process Regression (Algorithm 2.1 from Rasmussen).
+    Expects all vector inputs as numpy arrays.
+    """
+    X, y, k, sigma_n, x = inputs, targets, covariance_function, noise_level, test_inputs
+    K = compute_covariance(X, k)
+    n = K.shape[0]
+    L = np.linalg.cholesky(K + sigma_n * np.eye(n))
+    alpha = np.linalg.solve(L.T, np.linalg.solve(L, y))
+    m = test_inputs.size
+    mean, variance = np.zeros(m), np.zeros(m)
+    for i in range(m):
+        test_input = test_inputs[i]
+        test_cov = np.array([k(test_input, inputs[j]) for j in range(n)])
+        v = np.linalg.solve(L, test_cov)
+        mean[i] = test_cov.T @ alpha
+        variance[i] = k(test_input, test_input) - v.T @ v
+    log_marginal_likelihood = -0.5 * y.T @ alpha - np.sum(np.log(np.diag(L))) - 0.5 * n * np.log(2 * np.pi)
+    return mean, variance, log_marginal_likelihood
 
-    def __init__(self, 
-                mean_func=ZERO_MEAN, 
-                cov_func=SQUARED_EXP(1, 1),
-                initial_dataset=None,
-                noise_variance=1e-2):
-        self.mean_func = mean_func
-        self.cov_func = cov_func
-        self.noise_var = noise_variance
-        self.cov_mtx = None
-        self.dataset = (list(), list())
-        if initial_dataset is not None:
-            X, Y = initial_dataset
-            self.update(X, Y)
 
-    def update(self, new_X, new_Y):
-        """
-        Updates the GP to reflect the posterior given new training points.
-        """
-        X, Y = self.dataset
-        self._compute_cov(new_X)
-        X.extend(new_X)
-        Y.extend(new_Y)
-
-    def get_distribution(self, new_X):
-        """ 
-        Returns the mean and covariance of the 
-        multivariate Guassian distribution describing f(new_X).
-        """
-        X, Y = self.dataset
-        cov_mtx = self._compute_cov_mtx(new_X)
-        if self.cov_mtx is None:
-            n = cov_mtx.shape[0]
-            return np.array([self.mean_func(x) for x in new_X]), cov_mtx + self.noise_var * np.eye(n)
-        cov_ext = self._compute_cov_ext(new_X)
-        cov_inv = self._compute_cov_inv()
-        return np.array(cov_ext @ cov_inv @ np.array(Y).T), \
-                np.array(cov_mtx - cov_ext @ cov_inv @ cov_ext.T)
-
-    def predict(self, X):
-        """
-        Returns the MAP estimate of f(X) and associated covariance matrix.
-        """
-        mean, covariance = self.get_distribution(X)
-        return mean, covariance
-
-    def sample(self, X):
-        """
-        Returns a sampled function f*(X).
-        """
-        mean, covariance = self.get_distribution(X)
-        return np.random.multivariate_normal(mean, covariance)
-
-    def get_dataset(self):
-        return np.array(self.dataset[0]), np.array(self.dataset[1])
-
-    def _compute_cov(self, new_X):
-        new_cov_mtx = self._compute_cov_mtx(new_X)
-        if self.cov_mtx is None:
-            self.cov_mtx = new_cov_mtx
-        else:
-            n = self.cov_mtx.shape[0]
-            cov_ext = self._compute_cov_ext(new_X)
-            self.cov_mtx = np.hstack((np.vstack((self.cov_mtx, cov_ext)), np.vstack((cov_ext.T, cov_mtx))))
-    
-    def _compute_cov_ext(self, new_X):
-        X, Y = self.dataset
-        n = len(X)
-        new_n = len(new_X)
-        cov_ext = np.zeros((new_n, n))
-        for i in range(new_n):
-            for j in range(n):
-                cov_ext[i, j] = self.cov_func(new_X[i], X[j])
-        return cov_ext
-    
-    def _compute_cov_mtx(self, X):
-        n = len(X)
-        cov_mtx = np.zeros((n, n)) 
-        for i in range(n):
-            for j in range(i + 1):
-                cov = self.cov_func(X[i], X[j])
-                cov_mtx[i, j] = cov
-                cov_mtx[j, i] = cov
-        return cov_mtx
-
-    def _compute_noisy_cov(self):
-        n = self.cov_mtx.shape[0]
-        return self.cov_mtx + np.random.normal(0.0, np.sqrt(self.noise_var)) * np.eye(n)
-
-    def _compute_cov_inv(self):
-        # TODO: do something smarter with cholesky decomposition
-        return np.linalg.inv(self._compute_noisy_cov())
-
+def compute_covariance(X, k):
+    """
+    Compute covariance matrix for inputs X.
+    Assumes covariance function k is symmetric.
+    """
+    n = X.shape[0]
+    covariance = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i + 1):
+            cov = k(X[i], X[j])
+            covariance[i, j] = cov
+            covariance[j, i] = cov
+    return covariance
 
 
 if __name__ == "__main__":
