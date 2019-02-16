ade58afe49500f8cd1e6f59c29341c6a08b5b957
Brandon Baraban <bbaraban@mit.edu>
2019-02-16 17:42:57 -0500

    restructure input data

--- gp_regression.py
+++ gp_regression.py
@@ -12,17 +12,17 @@ def rbf(l_scale, sigma):
     c = -1.0 / (2.0 * np.power(l_scale, 2))
     def f(x, y):
         x_ = x - y
-        return sigma * np.exp(c * (x_ @ x_.T))
+        return sigma * np.exp(c * (x_.T @ x_))
     return f
 
 
 def plot_gpr_given_func(f=lambda x: np.sin(x), num_train=5):
     test_X = np.linspace(-5, 5, num=250) # 250 is arbitrary
-    X = np.random.choice(test_X, num_train, replace=False).reshape((-1, 1))
+    X = np.random.choice(test_X, num_train, replace=False).reshape((1, -1))
     y = f(X)
     fig, ax = plt.subplots(1)
     ax.plot(X, y, 'b+')
-    mean, variance, log_ml = gpr(X, y, rbf(1, 1), 1e-2, test_X.reshape((-1, 1)))
+    mean, variance, log_ml = gpr(X, y.T, rbf(1, 1), 1e-2, test_X.reshape((1, -1)))
     ax.plot(test_X, mean, 'red') # plot MAP estimate of f
     ax.plot(test_X, f(test_X), 'green') # plot f
     plot_confidence(test_X, mean, np.sqrt(variance), ax)
@@ -46,8 +46,8 @@ def gpr(inputs, targets, covariance_function, noise_level, test_inputs):
     m = test_inputs.size
     mean, variance = np.zeros(m), np.zeros(m)
     for i in range(m):
-        test_input = test_inputs[i]
-        test_cov = np.array([k(test_input, inputs[j]) for j in range(n)])
+        test_input = test_inputs[:, i]
+        test_cov = np.array([k(test_input, inputs[:, j]) for j in range(n)])
         v = np.linalg.solve(L, test_cov)
         mean[i] = test_cov.T @ alpha
         variance[i] = k(test_input, test_input) - v.T @ v
@@ -64,11 +64,11 @@ def compute_covariance(X, k):
     Compute covariance matrix for inputs X.
     Assumes covariance function k is symmetric.
     """
-    n = X.shape[0]
+    n = X.shape[1]
     covariance = np.zeros((n, n))
     for i in range(n):
         for j in range(i + 1):
-            cov = k(X[i:i+1], X[j:j+1])
+            cov = k(X[:, i:i+1], X[:, j:j+1])
             covariance[i, j] = cov
             covariance[j, i] = cov
     return covariance
